f5515d6a26e699488b453cf1e88e68cf81e94c45
Nimrod levy <El3ct71k@Gmail.com>
2014-11-09 20:41:28 +0200

    Updated

--- sandwormdetector.py
+++ sandwormdetector.py
@@ -4,35 +4,26 @@ __author__ = 'El3ct71k'
 import os
 import re
 import zipfile
-from shutil import copy, rmtree
 from argparse import ArgumentParser
-from tempfile import gettempdir
 
 def sandworm_detactor(name):
 	if not os.path.exists(name):
 		print("File not found")
 		exit(-1)
 	files = list()
-	copy(name, "exploit.zip")
+
 	try:
-		with open('exploit.zip', 'rb') as fh:
-			z = zipfile.ZipFile(fh)
-			for name in z.namelist():
-				z.extract(name, gettempdir())
-	except zipfile.BadZipfile:
-		print("Break file.")
-		exit(-1)
-	cur_files = '%s/ppt/embeddings/' % gettempdir()
-	locations = ['%s/oleObject1.bin' % cur_files, '%s/oleObject2.bin' % cur_files]
-	for loc in locations:
-		if os.path.exists(loc):
-			with open(loc, "rb") as ole:
-				mal_file = re.search(r"(?P<host>\\{2}.+([\\/].+)+)", str(ole.read()))
+		with zipfile.ZipFile(name, 'r') as z:
+			for f in ('ppt/embeddings/oleObject1.bin', 'ppt/embeddings/oleObject2.bin'):
+				mal_file = re.search(r"(?P<host>\\{2}.+([\\/].+)+)", z.read(f))
 				if mal_file:
-					files.append(mal_file.group('host').strip('\x00'))
-	os.remove('exploit.zip')
-	rmtree("%s/ppt" % gettempdir())
-	return files
+					files.append(mal_file.group('host'))
+	except zipfile.BadZipfile:
+		print("Not a .ppsx file.")
+	except KeyError:
+		print("Sandworm vector not found")
+	finally:
+		return files
 
 if __name__ == '__main__':
 	parser = ArgumentParser(prog=os.path.basename(__file__))
@@ -43,5 +34,3 @@ if __name__ == '__main__':
 		print("Sandworm vector attack detacted!\nEvil files:")
 		for malware in files:
 			print(malware)
-	else:
-		print("Sandworm vector not found")
